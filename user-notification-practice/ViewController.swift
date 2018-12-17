bb9ff3a4845d9bd6e9420bcf6ffa8ec447f69349
Shima <shima.jin@icloud.com>
2018-12-18 00:38:50 +0900

    Initial Commit

---
+++ user-notification-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  user-notification-practice
+//
+//  Created by Jinsei Shima on 2018/12/18.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
